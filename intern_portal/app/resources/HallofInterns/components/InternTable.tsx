315f7e013c85e7760af1e61a10715148c84b230f
Chavis <matthew.chavis@akima.com>
2024-07-19 11:41:28 -0500

    Made the table will work on constructing the rest of the data for the table in a bit gonna grab some pizza

--- intern_portal/app/resources/HallofInterns/components/InternTable.tsx
+++ intern_portal/app/resources/HallofInterns/components/InternTable.tsx
@@ -1,29 +1,59 @@
+"use client";
 import React from "react";
+import { useEffect, useState } from "react";
+import { Montserrat } from "next/font/google";
 
+const font = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 const InternTable = () => {
+  const [tables, setTables] = useState([]);
+
+  useEffect(() => {
+    fetch("/table.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("WOMP WOMP");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setTables(data);
+      });
+  }, []);
   return (
-    <div>
-      <div className="overflow-x-auto">
-        <table className="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Current Where Abouts</th>
-              <th>Internship</th>
-              <th>intern currently? </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-base-200">
-              <th>NA</th>
-              <td>NA</td>
-              <td>NA</td>
-              <td>NA</td>
-              <td>NA</td>
-            </tr>
-          </tbody>
-        </table>
+    <div className={`${font.className}`}>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>College</th>
+                <th>Internship</th>
+                <th>Where Now</th>
+                <th>intern currently? </th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            {tables.map((table) => (
+              <tbody key={table.ID}>
+                <tr className="bg-base-200">
+                  <th>{table.Name}</th>
+                  <td>{table.Date}</td>
+                  <td>{table.College}</td>
+                  <td>{table.Internship}</td>
+                  <td>{table.WhereNow}</td>
+                  <td>{table.current}</td>
+                  <td>{table.Email}</td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
       </div>
     </div>
   );
