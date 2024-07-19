659efece4c8fde8a255d38e73958c048a99c3981
Chavis <matthew.chavis@akima.com>
2024-07-18 20:01:20 -0500

    Added resource Page

---
+++ intern_portal/Scraper/pinnacleScraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.linkedin.com/company/pinnacle-solutions-inc./people/"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+}
+#Webscraper because I am very lazy
+try:
+    res = requests.get(url, headers=headers)
+    res.raise_for_status()  
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching URL: {e}")
+    exit()
+
+soup = BeautifulSoup(res.content, "html.parser")
+
+print(soup)
